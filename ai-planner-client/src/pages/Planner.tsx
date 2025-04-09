4fbbf81a1b3d0f69b0838b69014da5366a904129
bx <boweixu929@gmail.com>
2025-04-08 22:24:40 -0400

    refactored Planner

--- ai-planner-client/src/pages/Planner.tsx
+++ ai-planner-client/src/pages/Planner.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import GoalInput from '../components/GoalInput';
+import React from 'react'
+import useAxios from '@/utils/useAxios';
+import { endpoints } from '@/services/apis';
+import GoalCard from '@/components/GoalCard';
+import MyInput from '@/components/UI/MyInput';
+import { MyButton } from '@/components/UI/MyButton';
 
-const Planner: React.FC = () => {
+type MVPResponseType = {
+    start_time: string;
+    end_time: string;
+    title: string;
+}
+
+export default function Planner() {
+    const [goal, setGoal] = React.useState<string>('')
+    const { data, error, loading, refetch } = useAxios({
+        url: endpoints.createPlan.url,
+        config: {
+            method: endpoints.createPlan.method,
+            data: {
+                input: goal
+            }
+        },
+        manual: true
+    });
+
     return (
         <div>
-            <GoalInput />
+            <GoalCard goal={data as MVPResponseType | undefined} />
+            <MyInput type="text" placeholder='Enter your goal here...' className='w-full mb-4' onChange={(e) => setGoal(e.target.value)} />
+            <MyButton onClick={() => {
+                if (!goal || !goal.trim().length) return;
+                refetch();
+            }}>
+                Start Planning
+            </MyButton>
         </div>
-    );
-};
-
-export default Planner;
+    )
+}
