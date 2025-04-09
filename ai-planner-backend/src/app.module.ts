ac46e4a9317d67854ecd45b2aa2a4a7c9a4df3a1
bx <boweixu929@gmail.com>
2025-04-08 21:27:13 -0400

    added logger middleware

--- ai-planner-backend/src/app.module.ts
+++ ai-planner-backend/src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlannerModule } from './planner/planner.module';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [
@@ -20,4 +21,8 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
