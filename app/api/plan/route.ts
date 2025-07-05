a92e37dc6828e750ea38395c9d4dc97e63ebf08f
bx <boweixu929@gmail.com>
2025-07-05 02:10:00 -0400

    lint

--- app/api/plan/route.ts
+++ app/api/plan/route.ts
@@ -1,13 +1,13 @@
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 
-async function GET(req: NextRequest) {
+// async function GET(req: NextRequest) {
 
-}
+// }
 
-async function POST(req: NextRequest) {
+// async function POST(req: NextRequest) {
 
-}
+// }
 
-async function PUT(req: NextRequest) {
+// async function PUT(req: NextRequest) {
 
-}
+// }
