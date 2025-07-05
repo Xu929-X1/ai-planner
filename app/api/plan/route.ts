319122c654494c6bfa17fe721316231608dedc8a
bx <boweixu929@gmail.com>
2025-07-05 02:12:37 -0400

    added placeholder for plans

--- app/api/plan/route.ts
+++ app/api/plan/route.ts
@@ -1,13 +1,14 @@
-// import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-// async function GET(req: NextRequest) {
+async function GET() {
+    return NextResponse.json("Get plan by ID")
+}
 
-// }
+async function POST() {
+    return NextResponse.json("Create plan")
 
-// async function POST(req: NextRequest) {
+}
 
-// }
-
-// async function PUT(req: NextRequest) {
-
-// }
+async function PUT() {
+    return NextResponse.json("Update plan by ID")
+}
