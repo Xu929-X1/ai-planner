b4c8720cc627d3d0eec0cf3a4c862c1764f3c63b
bx <boweixu929@gmail.com>
2025-07-05 02:14:02 -0400

    satify needy linter

--- app/api/plan/route.ts
+++ app/api/plan/route.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 
-async function GET() {
+export async function GET() {
     return NextResponse.json("Get plan by ID")
 }
 
-async function POST() {
+export async function POST() {
     return NextResponse.json("Create plan")
 
 }
 
-async function PUT() {
+export async function PUT() {
     return NextResponse.json("Update plan by ID")
 }
