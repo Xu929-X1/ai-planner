bdd1bea56d0839882abfac1c8fd990371b94fc37
bx <boweixu929@gmail.com>
2025-09-02 11:02:54 -0400

    feat: add flow page with React Flow integration and new node types
    - Added `@xyflow/react` dependency for flow visualization.
    - Created `FlowPage` component to render a basic flow with nodes and edges.
    - Implemented `NodeWithActions` component for interactive nodes with action buttons.
    - Introduced `BaseNode` components for consistent styling of nodes.
    - Added `PlanCard` component to the playground page for displaying plans.
    - Updated `package.json` to include new dependencies and updated Next.js version.

---
+++ app/playground/page.tsx
@@ -0,0 +1,11 @@
+import PlanCard from '@/components/PlanCard'
+import React from 'react'
+
+export default function page() {
+    return (
+        <>
+            <PlanCard />
+            123
+        </>
+    )
+}
--- components/PlanCard.tsx
+++ components/PlanCard.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Card, CardDescription, CardHeader, CardTitle } from './UI/card'
+
 
 export default function PlanCard() {
     return (
-        <div>PlanCard</div>
+        <Card className='w-[30%]'>
+            <CardHeader>
+                <CardTitle className='text-left'>Plan Card</CardTitle>
+                <CardDescription className='text-left'>
+                    This is a plan card component. show priority, status, etc
+                </CardDescription>
+            </CardHeader>
+        </Card>
     )
 }
